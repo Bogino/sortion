7316756a61ef08f6a2a90a6fd62335258085031e
19089324 <ivbogino@sberbank.ru>
2022-12-06 23:19:37 +0300

    initial commit

---
+++ src/BubbleSorter.java
@@ -0,0 +1,11 @@
+//public class BubbleSorter implements Sorter {
+//
+//    @Override
+//    public void sort(int[] digitMassive) {
+//        for (int j = digitMassive.length - 1; j >= 0; j--){
+//            for (int i = 0; i < j; i++){
+//                if
+//            }
+//        }
+//    }
+//}
